a05865d70b75352f5751c4e305d593e3dc9350c9
mitchwang422 <34224967+mitchwang422@users.noreply.github.com>
2019-02-13 13:14:39 +1300

    Talent_Onboarding_Task1

---
+++ talentOnboarding/Scripts/react/index.jsx
@@ -0,0 +1,17 @@
+﻿import React from "react"
+import ReactDOM from "react-dom"
+
+export default class Example extends React.Component {
+
+    render() {
+        return (
+            <div>               
+                <p>Hello world!</p>                
+            </div>
+        )
+    }
+}
+
+
+const root = document.getElementById("root")
+ReactDOM.render(<Example />, root)
